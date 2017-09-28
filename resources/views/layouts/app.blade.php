62a85989f8f4672d5e8315c040820a0ee8663a1f
meg <oobridgeoo@gmail.com>
2017-09-28 08:04:09 +0000

    routes setting

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Task管理</title>
+</head>
+<body>
+    @yield('content')    
+</body>
+</html>
---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+@section
+    <h1>タスク管理一覧</h1>
+    
+@endsection
