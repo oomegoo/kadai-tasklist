c0455ccb3d9d6520eb44ac35d89a3629a8227bbc
meg <oobridgeoo@gmail.com>
2017-10-11 13:30:49 +0000

    fixed mistake

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('content)
+@section('content')
     <div class="text-center">
         <h1>Log in</h1>
     </div>
@@ -8,15 +8,15 @@
             {!! Form::open(['route' => 'login.post']) !!}
                 <div class="form-group">
                     {!! Form::label('email','Email') !!}
-                    {!! Form::email('email','old('email'),['class' => 'form-control']') !!}
+                    {!! Form::email('email',old('email'),['class' => 'form-control']) !!}
                 </div>
                 <div class="form-group">
                     {!! Form::label('password','Password') !!}
-                    {!! Form::password('password',['class' => 'form-conrol']) !!}
+                    {!! Form::password('password',['class' => 'form-control']) !!}
                 </div>
                 {!! Form::submit('Log in',['class' => 'btn btn-primary btn-block']) !!}
             {!! Form::close() !!}
-            <p>New User?{!! link_to_route('singup.get','Sign up now') !!}</p>
+            <p>New User?{!! link_to_route('signup.get','Sign up now') !!}</p>
         </div>
     </div>
 @endsection
